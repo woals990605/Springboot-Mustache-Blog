2f701da5e831500b90ba38364d6a40010e5b03e1
woals990605 <woals990605@naver.com>
2022-05-12 17:12:09 +0900

    11. 글쓰기 완료

--- src/main/java/site/metacoding/springbootmustacheblog/web/PostController.java
+++ src/main/java/site/metacoding/springbootmustacheblog/web/PostController.java
@@ -1,25 +1,47 @@
 package site.metacoding.springbootmustacheblog.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.springbootmustacheblog.domain.post.Post;
+import site.metacoding.springbootmustacheblog.domain.post.PostRepository;
+import site.metacoding.springbootmustacheblog.domain.user.User;
+
+@RequiredArgsConstructor
 @Controller
 public class PostController {
+
+    private final HttpSession session;
+    private final PostRepository postRepository;
 
     // GET 글쓰기 페이지 /post/writeForm - 인증 O
     @GetMapping("/s/post/writeForm")
     public String writeForm() {
+
+        if (session.getAttribute("principal") == null) {
+            return "redirect:/loginForm";
+        }
+
         return "post/writeForm";
     }
 
     // 메인 페이지 - 인증 X
     // GET 글 목록 페이지 /post/list/
     @GetMapping({ "/", "post/list" }) // { "/", "post/list" }로 쓰면 두 가지 방법으로 들어올 수 있음
-    public String list() {
+    public String list(Model model) {
+
+        // 1. postRepository의 findAll() 호출
+        // 2. model에 담기
+        // 3. mustache 파일에 뿌리기
+
         return "post/list";
     }
 
@@ -50,7 +72,23 @@ public class PostController {
 
     // POST 글 쓰기 /post -> 글 목록으로 가기 - 인증 O
     @PostMapping("/s/post")
-    public String post() {
+    public String post(Post post) {
+
+        // title, content null검사, 공백검사, 길이검사 ,,,,
+
+        if (session.getAttribute("principal") == null) {
+            return "redirect:/loginForm";
+        }
+
+        // id가 필요한데 오브젝트를 넣어도 될까? 된다 -> 알아서 id(PK)만 뽑아간다
+        // 오브젝트 안넣으면 FK연결 안된다
+        User principal = (User) session.getAttribute("principal");
+        post.setUser(principal);
+        // insert into post(title, content, userId)
+        // values(입력 받기, 입력 받기, 세션오브젝트의 PK)
+
+        postRepository.save(post);
+
         return "redirect:/"; // 다시 컨트롤러의 메서드를 찾아가는 것
     }
 }
