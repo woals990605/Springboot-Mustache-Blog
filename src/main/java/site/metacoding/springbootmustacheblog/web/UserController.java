b6627f01df42d8f53b21957ad005852a6e7d6a06
woals990605 <woals990605@naver.com>
2022-05-12 16:02:04 +0900

    2.유저 관련 UI 완성

--- src/main/java/site/metacoding/springbootmustacheblog/web/UserController.java
+++ src/main/java/site/metacoding/springbootmustacheblog/web/UserController.java
@@ -15,10 +15,11 @@ public class UserController {
         return "user/joinForm";
     }
 
+    // username=ssar&password=1234&email=ssar@nate.com (x-www-form-urlencoded 타입)
     // 회원가입 INSERT - 인증(로그인) X
     @PostMapping("/join")
     public String join() { // 행위, 페이지 아님
-        return "redirect:/user/loginForm"; // 로그인페이지 이동해주는 컨트롤러 메서드를 재활용
+        return "redirect:/loginForm"; // 로그인페이지 이동해주는 컨트롤러 메서드를 재활용
     }
 
     // 로그인 페이지 (정적) - 인증(로그인) X
@@ -27,14 +28,15 @@ public class UserController {
         return "user/loginForm";
     }
 
-    // 로그인 SELECT * FROM user WHERE username=? AND password=?
+    // 로그인 SELECT * FROM user WHERE username=? AND password=? -> 이런 메서드는 없으니까 직접
+    // 만들어줘야함
     // 원래 SELECT는 무조건 GET요청
     // 근데 로그인만 예외! POST요청
     // 이유 : 주소에 패스워드를 남길 수 없으니까!! 보안을 위해!!
     // 로그인 - 인증(로그인) X
     @PostMapping("/login")
     public String login() {
-        return "메인 페이지를 돌려주면 됨"; // PostController 만들고 수정하자
+        return "redirect:/";
     }
 
     // 유저 상세 페이지 (동적 -> DB연동 필요) - 인증(로그인) O
