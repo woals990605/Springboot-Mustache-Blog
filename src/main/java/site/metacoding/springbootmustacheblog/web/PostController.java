910bc96c6e9bcf12ca0b4df25c55c12cba1c9576
woals990605 <woals990605@naver.com>
2022-05-12 20:10:35 +0900

    25. 스크립트 공격 방어하기 마지막 COMMIT

--- src/main/java/site/metacoding/springbootmustacheblog/web/PostController.java
+++ src/main/java/site/metacoding/springbootmustacheblog/web/PostController.java
@@ -79,8 +79,11 @@ public class PostController {
 
         }
 
+        // <> -> &lt;&gt; 치환해주기
         String rawContent = postEntity.getContent();
-        String encContent = rawContent.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+        String encContent = rawContent
+                .replaceAll("<script>", "&lt;script&gt;")
+                .replaceAll("</script>", "&lt;/script&gt;");
         postEntity.setContent(encContent);
 
         model.addAttribute("post", postEntity);
